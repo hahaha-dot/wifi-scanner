6cab5c27af89f6cd2dd1c8dd6fcc04b253bd98a9
hahaha-dot <74753147+hahaha-dot@users.noreply.github.com>
2020-12-04 11:55:05 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,50 +1,15 @@
-#Use it legally
-#Don't use for illegal activities
-#! /usr/bin/env python3
+import os
+import subprocess
+from subprocess import PIPE, Popen
 
-import pyfiglet
-import sys
-import time
-   
-result = pyfiglet.figlet_format("Port-Scanner", font = "slant"  ) 
-print(result)
-x = "WRITTEN BY WONG SOON HONG"
-slowprint(x)
-timesleep(1)
-sys.stdout.flush()
-print("WELCOME", end='\n')
-print("Please choose option you want", end='\n')
-print("[1] Open port scanner", end='\n')
-print("[2] Exit", end='\n')
-input = input(int("==>\n"))
-if input == 1:
-   print("Success to run")
-else: 
-   sys.exit(0)
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-print(input("Enter format you want to scan", end='\n'))
-special = print("Enter the ip address of target's that you comfirmed")
-print("[2] Enter a file that contain ip address to scan")
-selection = input(int("==>")
-   if selection == 2
-      path = input("Enter the file location")
+try:
+   proc = Popen(['iwconfig'], stdout=PIPE, stderr=subprocess.STDOUT)
+except OSError:
+   sys.exit(1)
+hello = proc.communicate()[0]
+for line in hello:
+   yes = line.split('\n')
+   if len(line) => 1:
+      print("len(line) => 1")
    else:
-      pass
-real = open(path,'r')
-really = real.readlines()
-new = input(int("Enter port to scan"))
-   for port in range(1,1025): 
-      result = sock.connect_ex((special,new))
-        if result == 0:
-            print "Port {}: 	 Open".format(port)
-        sock.close()
-
-except KeyboardInterrupt:
-    print "You pressed Ctrl+C"
-    sys.exit()
-
-except socket.gaierror:
-    print 'Hostname could not be resolved. Exiting'
-    sys.exit()
-                  
-                  
+      print("len(line) < 1")
